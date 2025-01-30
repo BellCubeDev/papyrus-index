5fbbcd7613e72ed01773b0b9b7a06a8fb2bf0a28
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-01-29 22:13:34 -0500

    Add Guard()ing Logic (remove logic from Papyrus-only functions)

--- src/papyrus/parsing/PapyrusParserError.ts
+++ src/papyrus/parsing/PapyrusParserError.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import fs from "node:fs";
 import type { PapyrusScriptDiscoveredDocument } from "./parse-all-for-game";
 
 export class PapyrusParserError extends Error /* not SyntaxError because it's not a JS syntax error */ {
@@ -19,13 +20,17 @@ export class PapyrusParserError extends Error /* not SyntaxError because it's no
     public line: number;
     public column: number;
 
-    constructor(message: string, public readonly index: number, script: PapyrusScriptDiscoveredDocument) {
+    constructor(message: string, public readonly index: number, document: PapyrusScriptDiscoveredDocument & {isModified?: boolean}) {
         index--; // The index is one character further than the actual position of the problem
-        const { line, column } = PapyrusParserError.computeLineAndColumn(script, index);
-        super(`Error in Papyrus script "${path.basename(script.absolutePath)}":   ${message}\n\n    ${script.absolutePath}:${line}:${column} (character #${index + 1})\n`);
+        const { line, column } = PapyrusParserError.computeLineAndColumn(document, index);
+        if (document.isModified) {
+            document = {...document, absolutePath: `${document.absolutePath}.reference`};
+            fs.writeFileSync(document.absolutePath, document.sourceCode, 'utf8');
+        }
+        super(`Error in Papyrus script "${path.basename(document.absolutePath)}":   ${message}\n\n    ${document.absolutePath}:${line}:${column} (character #${index + 1})\n`);
         if (this.stack) {
             this.stack = this.stack.replace(/^[^\S\n]+at /um, (match) =>
-                `${match}[Papyrus Code] (${script.absolutePath}:${line}:${column})\n${match}`
+                `${match}[Papyrus Code] (${document.absolutePath}:${line}:${column})\n${match}`
             );
         }
         this.line = line;
