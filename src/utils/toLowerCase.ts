4814ea2ffbb7b9c7555ed1dbcfcdaa36845d1e73
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-04-04 12:12:42 -0400

    🔊 Improve error logging when toLowerCase() fails
    It's far too common for me to change types somewhere and suddenly be passing an erroneous value into toLowerCase(), so this improved logging should help.

--- src/utils/toLowerCase.ts
+++ src/utils/toLowerCase.ts
@@ -1,3 +1,8 @@
 export function toLowerCase<T extends string>(value: T) {
-    return value.toLowerCase() as Lowercase<T>;
+    try {
+        return value.toLowerCase() as Lowercase<T>;
+    } catch (e) {
+        console.error('Failed to convert', value, 'to lowercase:', e);
+        throw e;
+    }
 }
