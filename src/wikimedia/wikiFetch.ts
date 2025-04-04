b92283fa589c0f8b9d0e04b98b96368da99ad1e6
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-04-04 12:09:30 -0400

    🧑‍💻 Treat 403s encountered by MediaWiki API servers as 403s to our code
    Functionally, they're the same! Might as well treat them as such.

--- src/wikimedia/wikiFetch.ts
+++ src/wikimedia/wikiFetch.ts
@@ -113,7 +113,12 @@ async function wikiFetchGetInternalWithParseJsonAndHandleErrors(wiki: PapyrusWik
                     return await wikiFetchGetInternalWithParseJsonAndHandleErrors(wiki, path, retriesSoFar + 1, url);
                 }
             }
-            throw new Error(`[wikiFetchGetInternalWithParseJsonAndHandleErrors - ERROR_IN_JSON] Failed to fetch ${url}: ${json.error.info}\n${JSON.stringify(json.error)}`);
+            if (process.env.NODE_ENV === 'development' && json.error.info === 'Error contacting the Parsoid/RESTBase server (HTTP 403)') {
+                console.log(`wikiFetchGetInternalWithParseJsonAndHandleErrors: The remote MediaWiki instance API could not contact the Parsoid/RESTBase server, since it returned a 403 Forbidden. For URL: ${url}`);
+                return WIKI_FETCH_403FORBIDDEN;
+            } else {
+                throw new Error(`[wikiFetchGetInternalWithParseJsonAndHandleErrors - ERROR_IN_JSON] Failed to fetch ${url}: ${json.error.info}\n${JSON.stringify(json.error)}`);
+            }
         } else {
             throw new Error(`[wikiFetchGetInternalWithParseJsonAndHandleErrors - ERROR_IN_JSON] Failed to fetch ${url}: ${JSON.stringify(json.error)}`);
         }
