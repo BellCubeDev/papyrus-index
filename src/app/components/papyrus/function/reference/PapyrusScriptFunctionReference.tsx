09a3eb9a1a6e0f36ce7e795b8f28fcf59ef3bf7c
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-04-06 08:53:58 -0400

    ⚰️ Remove an Unused Import
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/components/papyrus/function/reference/PapyrusScriptFunctionReference.tsx
+++ src/app/components/papyrus/function/reference/PapyrusScriptFunctionReference.tsx
@@ -1,4 +1,4 @@
-import { Suspense, type ComponentProps } from "react";
+import { type ComponentProps } from "react";
 import type { PapyrusScriptFunctionIndexed, PapyrusScriptFunctionIndexedAggregate } from "../../../../../papyrus/data-structures/indexing/function";
 import type { PapyrusGame } from "../../../../../papyrus/data-structures/pure/game";
 import { UnreachableError } from "../../../../../UnreachableError";
