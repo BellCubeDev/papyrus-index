af85e0c83397251cbe13822118956e6147271181
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-04-09 23:31:47 -0400

    ✨ Script Reference Tooltips Now Display Parent Script

--- src/app/components/papyrus/script/PapyrusScriptReferenceTooltip.tsx
+++ src/app/components/papyrus/script/PapyrusScriptReferenceTooltip.tsx
@@ -4,21 +4,27 @@ import styles from './PapyrusScriptReference.module.scss';
 import type { PapyrusPossibleScripts, PapyrusScriptIndexed, PapyrusScriptIndexedAggregate } from "../../../../papyrus/data-structures/indexing/script";
 import type { PapyrusScriptBySources } from "../../../../papyrus/data-structures/indexing/game";
 import { UnreachableError } from "../../../../UnreachableError";
-import { getScriptNameFromProps } from "./PapyrusScriptReference";
+import { getScriptNameFromProps, PapyrusScriptReference } from "./PapyrusScriptReference";
+import { joinJSX } from "../../../../utils/joinJSX";
+import { getBestName } from "../../../../utils/getBestName";
 
 export function PapyrusScriptReferenceTooltip<TGame extends PapyrusGame>(propsObj:
-    | {readonly script: |PapyrusScriptIndexed<TGame>, readonly scriptAggregate?: undefined, readonly possibleScripts?: undefined}
-    | {readonly script?: undefined, readonly scriptAggregate: PapyrusScriptIndexedAggregate<TGame>, readonly possibleScripts?: undefined}
-    | {readonly script?: undefined, readonly scriptAggregate?: undefined, readonly possibleScripts: typeof UnknownPapyrusScript | PapyrusPossibleScripts<TGame> | PapyrusScriptBySources<TGame>, missingName: string|null}
+    {
+        game: TGame,
+    } & (
+        | {readonly script: |PapyrusScriptIndexed<TGame>, readonly scriptAggregate?: undefined, readonly possibleScripts?: undefined}
+        | {readonly script?: undefined, readonly scriptAggregate: PapyrusScriptIndexedAggregate<TGame>, readonly possibleScripts?: undefined}
+        | {readonly script?: undefined, readonly scriptAggregate?: undefined, readonly possibleScripts: typeof UnknownPapyrusScript | PapyrusPossibleScripts<TGame> | PapyrusScriptBySources<TGame>, missingName: string|null}
+    )
 ): React.ReactElement {
-    const {script, scriptAggregate, possibleScripts} = propsObj;
+    const {game, script, scriptAggregate, possibleScripts} = propsObj;
     const name = getScriptNameFromProps(propsObj);
     if (script) {
         return <div className={styles.tooltip}>
             {name} (
             {script.isNative === null
-                ? script.extendsName
-                    ? `extends ${script.extendsName}`
+                ? script.extends
+                    ? <>extends <PapyrusScriptReference game={game} possibleScripts={script.extends} missingName={script.extendsName} /></>
                     : 'top-level'
                 : script.isNative
                     ? 'Native'
@@ -31,7 +37,7 @@ export function PapyrusScriptReferenceTooltip<TGame extends PapyrusGame>(propsOb
             {
                 scriptAggregate.isNative.some(([, isNative]) => isNative === null)
                     ? scriptAggregate.extendsName.some(([, extendsName]) => extendsName)
-                        ? `extends ${scriptAggregate.extendsName.map(n=>n[1]).join(' | ')}`
+                        ? <>extends {...joinJSX(' | ', scriptAggregate.extends.filter((n)=> n[1] !== null).map(n=><PapyrusScriptReference key={n[0].join('|')} game={game} possibleScripts={n[1]!} missingName={null} />))}</>
                         : 'top-level'
                     : scriptAggregate.isNative.every(([, isNative]) => !isNative)
                         ? 'Papyrus-Only'
@@ -47,9 +53,20 @@ export function PapyrusScriptReferenceTooltip<TGame extends PapyrusGame>(propsOb
                 This script is not included in the Papyrus Index. It is likely from a mod that is not indexed, or the script was not deemed relevant to the index. If you believe this is a mistake, please contact BellCube!
             </div>;
         }
+        const possibleScriptsEntries = Object.entries(possibleScripts);
         return <div className={styles.tooltip}>
-            {Array.from(new Set(Object.values(possibleScripts).map(s=>s.namespaceName))).join(' | ')} (
-
+            {getBestName(possibleScriptsEntries.map(v => v[1].namespaceName))} (
+            {
+                possibleScriptsEntries.some(([,s])=>s.isNative === null)
+                    ? Object.values(possibleScripts).some(s=>s.extendsName)
+                        ? <>extends {...joinJSX(' | ', possibleScriptsEntries.filter(([,s])=>s.extends !== null).map(([k, s])=><PapyrusScriptReference key={k} game={game} possibleScripts={s.extends!} missingName={s.extendsName} />))}</>
+                        : 'top-level'
+                    : Object.values(possibleScripts).every(s=>!s.isNative)
+                        ? 'Papyrus-Only'
+                        : Object.values(possibleScripts).every(s=>s.isNative)
+                            ? 'Native'
+                            : 'Native in some sources, Papyrus-Only in others'
+            }
             )
         </div>;
     } else {
