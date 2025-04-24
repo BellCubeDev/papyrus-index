bfbd67874be6700644952d08ca248134dbd643ed
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-04-24 17:59:38 -0400

    💬 Update <SourceName> Component's Vanilla Game Text

--- src/app/components/papyrus/SourceName.tsx
+++ src/app/components/papyrus/SourceName.tsx
@@ -1,10 +1,11 @@
 import type { PapyrusGame } from "../../../papyrus/data-structures/pure/game";
 import { PapyrusSourceType, type PapyrusScriptSourceMetadataExternal, type PapyrusScriptSourceMetadataVanilla } from "../../../papyrus/data-structures/pure/scriptSource";
+import { getGameName } from "../../../utils/getGameName";
 
 export function SourceName({source, long}: {source: PapyrusScriptSourceMetadataVanilla<PapyrusGame> | PapyrusScriptSourceMetadataExternal<PapyrusGame>, long?: boolean}): string {
     switch (source.type) {
         case PapyrusSourceType.Vanilla:
-            return long ? 'The Vanilla Game' : 'Vanilla';
+            return long ? `vanilla ${getGameName(source.game)}` : 'Vanilla';
         default:
             return long ? source.nameProper : source.nameShort;
     }
