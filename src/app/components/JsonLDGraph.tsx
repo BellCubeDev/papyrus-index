839775ade50b6d621abab50c370c50b852d183a7
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-08-30 20:26:03 -0400

    🩹 Make <JsonLDGraph> use "@graph"

--- src/app/components/JsonLDGraph.tsx
+++ src/app/components/JsonLDGraph.tsx
@@ -4,8 +4,7 @@ import 'server-only';
 export function JsonLDGraph({data}: {readonly data: Graph['@graph']}) {
     // eslint-disable-next-line react/no-danger -- we're using JSON.stringify to set a <script> tag's contents; it's fine
     return <script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify({
-        "@context": "http://schema.org",
-        "@type": "Graph",
-        graph: data,
+        "@context": "https://schema.org",
+        "@graph": data,
     }) }} />;
 }
