50c575ad993e83fd090d4f04a0bf8c65965c5148
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2026-06-26 23:00:09 -0400

    🔍 Remove <h1> from logo element

--- src/app/components/logo/ThePapyrusIndexLogo.tsx
+++ src/app/components/logo/ThePapyrusIndexLogo.tsx
@@ -1,11 +1,11 @@
 import styles from './ThePapyrusIndexLogo.module.scss';
 
 export function ThePapyrusIndexLogo() {
-    return <div className={styles.title}>
-        <h1>THE <span className={styles.papyrus}>
+    return <div className={styles.title}><div>
+        THE <span className={styles.papyrus}>
             <span>PAPYRUS</span>
             <span hidden>PAPYRUS</span> {/* Here for styling purposes */}
             <span hidden>PAPYRUS</span>
-        </span> INDEX</h1>
-    </div>;
+        </span> INDEX
+    </div></div>;
 }
